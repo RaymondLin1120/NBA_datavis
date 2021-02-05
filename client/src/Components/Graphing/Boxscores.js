7412c281961070a35ae7bb949785c756b318be89
Raymond Lin <Raymond.L_@hotmail.com>
2021-02-05 00:17:03 -0500

    Boxscore added in

--- client/src/Components/Graphing/Boxscores.js
+++ client/src/Components/Graphing/Boxscores.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useTable } from 'react-table';
+import { useTable, usePagination } from 'react-table';
 
 const columns = [
     {
@@ -7,65 +7,170 @@ const columns = [
         accessor: 'date'
     },
     {
-        Headers: 'Matchup',
+        Header: 'Matchup',
         accessor: 'matchup'
     },
     {
-        Headers: 'Result',
+        Header: 'Result',
         accessor: 'wl'
     },
     {
-        Headers: 'FG%',
+        Header: 'FG%',
         accessor: 'fgPct'
     },
     {
-        Headers: 'FG3%',
+        Header: 'FG3%',
         accessor: 'fg3Pct'
     },
     {
-        Headers: 'FT%',
+        Header: 'FT%',
         accessor: 'ftPct'
     },
     {
-        Headers: 'MIN',
+        Header: 'MIN',
         accessor: 'min'
     },
     {
-        Headers: 'REB',
+        Header: 'REB',
         accessor: 'reb'
     },
     {
-        Headers: 'AST',
+        Header: 'AST',
         accessor: 'ast'
     },
     {
-        Headers: 'BLK',
+        Header: 'BLK',
         accessor: 'blk'
     },
     {
-        Headers: 'STL',
+        Header: 'STL',
         accessor: 'stl'
     },
     {
-        Headers: 'TOV',
+        Header: 'TOV',
         accessor: 'tov'
     },
     {
-        Headers: 'PF',
+        Header: 'PF',
         accessor: 'pf'
     },
     {
-        Headers: 'PTS',
+        Header: 'PTS',
         accessor: 'pts'
     }
 ]
 
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page, // Instead of using 'rows', we'll use page,
+    // which has only the rows for the active page
+
+    // The rest of these things are super handy, too ;)
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0 },
+    },
+    usePagination
+  )
+
+  // Render the UI for your table
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      {/* 
+        Pagination can be built however you'd like. 
+        This is just a very basic UI implementation:
+      */}
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(page)
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={e => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[5, 10, 15, 20, 30].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
+  )
+}
 
 function Boxscores(data) {
-
     return (
         <div>
-            
+            <Table columns={columns} data={data.data} /> 
         </div>
     )
 }
