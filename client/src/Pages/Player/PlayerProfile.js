d92813e4548eae4b9f5c19007625604d390fb4fd
raymondchuu <raymondchu1996@gmail.com>
2021-02-12 21:36:37 -0500

    updating player headers

--- client/src/Pages/Player/PlayerProfile.js
+++ client/src/Pages/Player/PlayerProfile.js
@@ -1,14 +1,46 @@
 import React, { useEffect } from 'react';
-import nba from 'nba'
+
+const teamColor = {
+    atl: '#E03A3E',
+    bkn: '#000000',
+    bos: '#007A33',
+    cha: '#00788C',
+    chi: '#CE1141',
+    cle: '#860038',
+    dal: '#00538C',
+    den: '#0E2240',
+    det: '#C8102E',
+    gsw: '#1D428A',
+    hou: '#CE1141',
+    ind: '#002D62',
+    lac: '#C8102E',
+    lal: '#552583',
+    mem: '#5D76A9',
+    mia: '#98002E',
+    mil: '#00471B',
+    min: '#0C2340',
+    nop: '#0C2340',
+    nyk: '#006BB6',
+    okc: '#007AC1',
+    orl: '#0077C0',
+    phi: '#006BB6',
+    phx: '#1D1160',
+    por: '#E03A3E',
+    sac: '#5A2D81',
+    sas: '#C4CED4',
+    tor: '#CE1141',
+    uta: '#002B5C',
+    was: '#002B5C'
+}
 
 export default function PlayerProfile(props) {
     
     let cats = ['min', 'pts', 'reb', 'ast', 'stl', 'blk']
     let statData = props.seasonStats[0]
     console.log(props.playerInfo)
-    console.log(statData)
+    console.log(props)
     return (
-        <section className="playerInfo-container" style={{background: "#552583", backgroundImage: "url(https://www.nba.com/stats/media/img/teams/logos/LAL_logo.svg)", backgroundRepeat: 'no-repeat', backgroundSize: "contain", backgroundAttachment: 'fixed', backgroundPosition: '0% 0%', backgroundBlendMode: "soft-light" }} >
+        <section className="playerInfo-container" style={{background: teamColor[props.playerInfo[0].teamAbbreviation.toLowerCase()], backgroundImage: `url(https://www.nba.com/stats/media/img/teams/logos/${props.playerInfo[0].teamAbbreviation}_logo.svg)`, backgroundRepeat: 'no-repeat', backgroundSize: "contain", backgroundAttachment: 'fixed', backgroundPosition: '0% 0%', backgroundBlendMode: "soft-light" }} >
 
             <div className="playerInfo">
                 <div style={{display: 'flex', flexDirection: 'row'}}>
